774b3b1f627864b77de042714d77506f477d839b
Ana Mari Noel <anamari.noel@ymail.com>
2018-10-25 23:31:01 +0200

    Task 1. finished

---
+++ 1_oddNumbers.js
@@ -0,0 +1,20 @@
+function oddNumbers (first, second)
+{
+    var i,
+        min = first,
+        max = second,
+        oddNumbers = [];
+
+    if (second < first) {
+        min = second;
+        max = first;
+    }
+
+    for (i = min; i <= max; i++) {
+        if (i%2 !== 0) {
+            oddNumbers.push(i);
+        }
+    }
+
+    return oddNumbers;
+}
